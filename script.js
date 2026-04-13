3ccb95fc824617e460dbfd15aebc16d53bfa3439
Harsh-790 <wellharsh8298@gmail.com>
2026-04-13 15:30:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ function orderNow(button) {
     let image = cake.querySelector("img").src;
 
     let phoneNumber = "917903545280"; 
-
     let message = `Hello, I want to order this cake:
 
 📌 Name: ${name}
 💰 Price: ${price}
 🖼 Image: ${image}`
-
     let url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
     window.open(url, "_blank");
@@ -28,35 +26,27 @@ function sendToWhatsApp() {
         alert("Please fill all fields!");
         return;
     }
-
-    let whatsappNumber = "917903545280"; // 👉 apna number (without +)
-
+    let whatsappNumber = "917903545280";
     let text = `New Contact Form Message
-
 Name: ${name}
 Phone: ${phone}
 Address: ${address}
 Message: ${message}`;
-
     let url = "https://wa.me/" + whatsappNumber + "?text=" + encodeURIComponent(text);
-
     window.open(url, "_blank");
 }
 
 const pages = ["index.html", "cake.html", "kulfi_house.html"];
 let docs = [];
 
-// load pages once
 pages.forEach(p =>
     fetch(p)
     .then(r => r.text())
     .then(t => docs.push({ p, d: new DOMParser().parseFromString(t, "text/html") }))
 );
 
-// search function
 function search(q) {
     let html = "";
-
     docs.forEach(({ p, d }) => {
         d.querySelectorAll(".cakes").forEach(c => {
             let name = c.querySelector(".CakePrice")?.innerText.toLowerCase();
@@ -71,11 +61,7 @@ function search(q) {
     document.getElementById("searchGrid").innerHTML =
         html || "<p style='color:white;'>No results</p>";
 }
-
-// live search
 document.getElementById("search").oninput = e =>
     search(e.target.value.toLowerCase());
-
-// button search
 document.querySelector(".btn-sm").onclick = () =>
     search(document.getElementById("search").value.toLowerCase());
